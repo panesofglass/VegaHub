a9c19d5b85ef501cf54828b889f795ef52e1a95b
Ryan Riley <ryan.riley@panesofglass.org>
2013-08-08 11:55:46 -0500

    Baseline client server communication.

---
+++ ConsoleApplication5/Program.fs
@@ -0,0 +1,46 @@
+﻿open System
+open Owin
+open Microsoft.AspNet.SignalR
+open Microsoft.Owin.Hosting
+open ImpromptuInterface.FSharp
+
+let addMessage message (target: obj) =
+    target?addMessage(message)
+
+let addData data (target: obj) =
+    target?addData(data)
+
+type ChartHub() =
+    inherit Hub()
+    member x.Send(message: string) : unit =
+        x.Clients.All |> addMessage message
+
+let attachHub (app: IAppBuilder) =
+    let config = new HubConfiguration(EnableJSONP = true)
+    app.MapSignalR(config) |> ignore
+
+let sendMessage message (hub: IHubContext) =
+    hub.Clients.All |> addMessage message
+
+let sendData data (hub: IHubContext) =
+    hub.Clients.All |> addData data
+
+[<EntryPoint>]
+let main argv = 
+    let url = "http://localhost:8081"
+    use __ = WebApp.Start(url, Action<_> attachHub)
+    Console.WriteLine("Running chart hub on " + url)
+
+    let hub = GlobalHost.ConnectionManager.GetHubContext<ChartHub>()
+
+    Console.WriteLine("Press any key to send a message.")
+    Console.ReadKey() |> ignore
+    hub |> sendMessage "Hello, Mathias!"
+
+    Console.WriteLine("Press any key to send data.")
+    Console.ReadKey() |> ignore
+    hub |> sendData [|1..10|]
+
+    Console.WriteLine("Press any key to stop.")
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ ConsoleApplication5/Scripts/app.js
@@ -0,0 +1,37 @@
+﻿(function (document, $, undefined) {
+    "use strict";
+
+    $(document).ready(function () {
+        var chartHub;
+
+        $.connection.hub.url = 'http://localhost:8081/signalr';
+
+        chartHub = $.connection.chartHub;
+        if (chartHub) {
+            // Define the `addMessage` function on the client.
+            chartHub.client.addMessage = function (message) {
+                $('#results').append('<p>' + message + '</p>');
+                console.log('Server called addMessage(' + message + ')');
+            };
+
+            // Define the `addData` function on the client.
+            chartHub.client.addData = function (data) {
+                console.log('Server called addData(' + data + ')');
+                data.forEach(function (item) {
+                    $('#results').append('<p>' + item + '</p>');
+                });
+            };
+
+            // Kick off the hub.
+            $.connection.hub.start().
+              done(function () {
+                  $('#submit').on('click', function () {
+                      chartHub.server.send($('#source').val());
+                  });
+              });
+        }
+        else {
+            console.log('No hub found by the name of chartHub');
+        }
+    });
+})(document, jQuery);
