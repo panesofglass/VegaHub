0500f70b99db9a00302bf12001cac371fae52803
Ryan Riley <ryan.riley@panesofglass.org>
2013-08-09 11:31:49 -0500

    Make VegaHub a class library and move the sample code to a script file.

---
+++ src/Script.fsx
@@ -0,0 +1,35 @@
+﻿#I """..\packages"""
+#r """Owin.1.0\lib\net40\Owin.dll"""
+#r """Microsoft.Owin.2.0.0-rtw1-20808-529-dev\lib\net45\Microsoft.Owin.dll"""
+#r """Microsoft.Owin.Diagnostics.2.0.0-rtw1-20808-529-dev\lib\net40\Microsoft.Owin.Diagnostics.dll"""
+#r """Microsoft.Owin.FileSystems.0.24.0-pre-20808-531-dev\lib\net40\Microsoft.Owin.FileSystems.dll"""
+#r """Microsoft.Owin.Hosting.2.0.0-rtw1-20808-529-dev\lib\net45\Microsoft.Owin.Hosting.dll"""
+#r """Microsoft.Owin.Security.2.0.0-rtw1-20808-529-dev\lib\net45\Microsoft.Owin.Security.dll"""
+#r """Microsoft.Owin.StaticFiles.0.24.0-pre-20808-531-dev\lib\net40\Microsoft.Owin.StaticFiles.dll"""
+#r """Microsoft.Owin.Host.HttpListener.2.0.0-rtw1-20808-529-dev\lib\net45\Microsoft.Owin.Host.HttpListener.dll"""
+#r """Newtonsoft.Json.5.0.6\lib\net45\Newtonsoft.Json.dll"""
+#r """Microsoft.AspNet.SignalR.Core.2.0.0-rtm1-130808-b140\lib\net45\Microsoft.AspNet.SignalR.Core.dll"""
+#r """ImpromptuInterface.6.2.2\lib\net40\ImpromptuInterface.dll"""
+#r """ImpromptuInterface.FSharp.1.2.13\lib\net40\ImpromptuInterface.FSharp.dll"""
+#load "WebApp.fs"
+#load "Vega.fs"
+
+open System
+open VegaHub
+open VegaHub.Vega
+
+let disposable = WebApp.launch "http://localhost:8081"
+
+// Simulate real-time updates
+let rand = Random(42)
+let rec loop data iter = async {
+    let data' = Array.append data [| {x = data.Length; y = rand.Next(0, 100)} |]
+    data' |> Vega.bar
+    do! Async.Sleep 100
+    if iter = 0 then () else
+    return! loop data' <| iter - 1
+}
+
+loop [||] 25 |> Async.RunSynchronously
+
+disposable.Dispose()
---
+++ src/WebApp.fs
@@ -0,0 +1,28 @@
+﻿module VegaHub.WebApp
+
+open System
+open System.Diagnostics
+open Owin
+open Microsoft.AspNet.SignalR
+open Microsoft.Owin.Hosting
+open Newtonsoft.Json
+
+type ChartHub() =
+    inherit Hub()
+
+let private attachHub (app: IAppBuilder) =
+    let config = new HubConfiguration(EnableJSONP = true)
+    app.MapSignalR(config)
+
+let private hostFiles (app: IAppBuilder) =
+    // TODO: Prefer default files.
+    //app.UseDefaultFiles __SOURCE_DIRECTORY__
+    // TODO: either pass in the path or detect it properly. FSI makes this hard.
+    app.UseStaticFiles __SOURCE_DIRECTORY__
+
+let launch (url: string) =
+    let disposable = WebApp.Start(url, attachHub >> hostFiles >> ignore)
+    Console.WriteLine("Running chart hub on " + url)
+    // TODO: Use canopy?
+    Process.Start(url + "/index.html") |> ignore
+    disposable
